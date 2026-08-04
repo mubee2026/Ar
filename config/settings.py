24b05debcb5b2be7cff80cffa9473a3a7768caf5
mubee2026 <silenteyes8676@gmail.com>
2026-08-04 17:51:04 +0400

    Connect PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,10 +143,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # --------------------------------------------------
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
